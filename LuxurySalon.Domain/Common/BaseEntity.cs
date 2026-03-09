511eac3532ad3fb29d3bd0a9edd02ab31c34870c
Crazybitlisi <aymuhammed292@gmail.com>
2026-03-09 10:55:35 +0300

    Remove BaseEntity class definition

--- LuxurySalon.Domain/Common/BaseEntity.cs
+++ LuxurySalon.Domain/Common/BaseEntity.cs
@@ -1,14 +1,1 @@
-using System;
-
-namespace LuxurySalon.Domain.Common
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public string? CreatedBy { get; set; }
-        public DateTime? LastModifiedAt { get; set; }
-        public string? LastModifiedBy { get; set; }
-        public bool IsDeleted { get; set; } = false;
-    }
-}
+hi
